e39524fc36bc0c2aecd6db1401642140f9d98d91
Francesco Cioria <firefelix@gmail.com>
2019-02-11 12:44:24 +0100

    fix it

--- fuckrep.js
+++ fuckrep.js
@@ -1,29 +1,23 @@
-var period = 1000, nretries = 6;
+function swapArticles() {
+  try {
+    const freeArticleElement = document
+      .querySelector("news-app")
+      .shadowRoot.querySelector("news-article")
+      .shadowRoot.querySelector(".amp-doc-host")
+      .shadowRoot.querySelector(
+        '.detail-article_body > div[subscriptions-section="content-not-granted"]'
+      );
 
-function httpGet(theUrl) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-  xmlHttp.send( null );
-  return xmlHttp.responseText;
-}
+    const completeArticleElement = document
+      .querySelector("news-app")
+      .shadowRoot.querySelector("news-article")
+      .shadowRoot.querySelector(".amp-doc-host")
+      .shadowRoot.querySelector(".detail-article_body > div.paywall");
 
-function fill(content, n) {
-  var a = document.getElementsByClassName('detail-article_body');
-  if (a.length > 0) {
-    a[0].innerHTML = content;
-  } else if (n < nretries) {
-    setTimeout(function() { fill(content, n + 1) }, period);
-  } else {
-    document.body.innerHTML = content;
+    freeArticleElement.innerHTML = completeArticleElement.innerHTML;
+  } catch (e) {
+    setTimeout(swapArticles, 500);
   }
 }
 
-function fuckrep() {
-  var pathname = window.location.pathname;
-  var url = 'https://rep.repubblica.it/ws/detail/' + pathname.substring(5) + 'content.json';
-  var data = JSON.parse(httpGet(url));
-  var content = data[0].content + '<h2>FuckRep!</h2>';
-  fill(content, 0);
-}
-
-fuckrep();
+swapArticles();
